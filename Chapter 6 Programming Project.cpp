255c095e92f98854a0b93e8d3569513f02e7a757
legod <legod@BRICK>
2025-07-23 00:33:43 -0400

    no comments

--- Chapter 6 Programming Project.cpp
+++ Chapter 6 Programming Project.cpp
@@ -2,10 +2,100 @@
 //
 
 #include <iostream>
+using namespace std; 
+
+void getLength_Width(double &length, double &width) {
+    
+    do {
+        cout << "What is length and width of  structure? \n";
+        cin >> length;
+        cin >> width;
+
+
+
+        if (length <= 0 || width <= 0) {
+
+            cout << "Error, please input positive value\n";
+
+
+
+
+
+
+        }
+
+
+
+    }
+
+
+
+    while (length <= 0 || width <= 0);
+
+
+    
+}
+
+
+
+
+
+double calcPerimeter(double L,double W) {
+
+    return 2 * (L + W);
+
+
+}
+
+double calcArea(double l, double w) {
+
+    return (l * w);
+
+
+
+}
+
+void displayProperties(double PE, double AREA) {
+
+    cout << "Perimeter:\t" << PE << endl;
+    cout << "Area:     \t" << AREA << endl;
+
+
+
+
+
+}
+
+
+
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    char choice;
+
+    do {
+        double length, width;
+
+        getLength_Width(length, width);
+
+        displayProperties(calcPerimeter(length, width), calcArea(length, width));
+
+
+
+        cout << "Process another? (Y/N): \n";
+            cin >> choice;
+
+
+    } 
+    
+    
+    while (choice == 'Y');
+
+
+    return 0;
+
+
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
